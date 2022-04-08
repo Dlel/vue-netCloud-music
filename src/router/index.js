b64470f1f8631d564e39f0512faba28c4269689d
Dlel <62826161+Dlel@users.noreply.github.com>
2022-04-08 14:29:33 +0800

    添加baseUrl+修改注释

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
-/* test */
+/* 测试*/
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Find from '../views/find/Find.vue'
 
+const baseUrl = 'base.com'
 const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err)
 }
 
 Vue.use(VueRouter)
-
+console.log(baseUrl)
 const routes = [{ // 默认先进去的地址
     path: '/',
     redirect: '/find'
