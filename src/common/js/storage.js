071ce4631047d96c891f9b563fcfcd1ce1d4f5d5
Dlel <62826161+Dlel@users.noreply.github.com>
2022-04-09 17:16:52 +0800

    添加common

---
+++ src/common/js/storage.js
@@ -0,0 +1,20 @@
+import storage from 'good-storage'
+
+const KEY_SELLER = '__seller__'
+
+export function setStorage (id, key, val) { // id ：店铺  key：获取的内容关键字  val：要存在这个关键字的值
+  const seller = storage.get(KEY_SELLER, {}) // 获取有关KEY_SELLER的缓存，如果没有定义就获取空对象
+  if (!seller[id]) { // 获取到缓存，看也没有对应的店铺id
+    seller[id] = {}
+  }
+  seller[id][key] = val
+  storage.set(KEY_SELLER, seller)
+}
+
+export function loadStorage (id, key, def) { // id ：店铺  key：获取的内容关键字  val：要存在这个关键字的值
+  const seller = storage.get(KEY_SELLER, {}) // 获取有关KEY_SELLER的缓存，如果没有定义就获取空对象
+  if (!seller[id]) { // 获取到缓存，看也没有对应的店铺id
+    return def
+  }
+  return seller[id][key] || def
+}
