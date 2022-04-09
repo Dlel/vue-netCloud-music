e01713f0bf29297ca5a1866ed700d34d9c1db422
Dlel <62826161+Dlel@users.noreply.github.com>
2022-04-09 19:01:34 +0800

    删除ab常量

--- src/common/js/storage.js
+++ src/common/js/storage.js
@@ -1,8 +1,6 @@
 import storage from 'good-storage'
 
 const KEY_SELLER = '__seller__'
-let b = '456'
-let a = '123'
 export function setStorage(id, key, val) { // id ：店铺  key：获取的内容关键字  val：要存在这个关键字的值
   const seller = storage.get(KEY_SELLER, {}) // 获取有关KEY_SELLER的缓存，如果没有定义就获取空对象
   if (!seller[id]) { // 获取到缓存，看也没有对应的店铺id
